f2511d65fdfa7f869485fc865942a0accaa41850
David Valentin Möhring <davidvalentinsemail@gmail.com>
2016-10-03 22:09:27 +0200

    second commit

---
+++ src/scripts/app.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @name
+   *
+   * @description
+   *
+   */
+  angular.module(
+      'motion',
+
+      // DEPENDENCIES
+      [
+
+        //'dvm.templates',
+      ]);
+
+}());
---
+++ src/scripts/main/motionMain.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+
+    /**
+     * @ngdoc directive
+     * @name motion.main.components:main
+     * @description
+     *
+     */
+
+    angular.module('motion').component('main', {
+            bindings: {
+            },
+            controller: mainCtrl,
+            controllerAs: 'mainCtrl',
+            templateUrl: 'src/scripts/main/mainCtrl.tpl.html'
+        }
+    );
+
+    /**
+     * @ngdoc controller
+     * @name motion.main.components:mainCtrl
+     * @description
+     *
+     */
+    function mainCtrl() {
+
+        var self = this;
+
+
+        
+
+
+    }
+})();
